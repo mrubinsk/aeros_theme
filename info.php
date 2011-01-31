6bd25c8bc9aa6b8bffd8bdb936fc4d896c8735ae
Michael J. Rubinsky <mrubinsk@horde.org>
2011-01-31 14:13:54 -0500

    Initial commit from CVS

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Aeros theme for Horde
+ *
+ * Aviation based theme with colors derived from VFR sectional charts
+ *
+ * $Id: info.php,v 1.2 2007/06/21 14:17:26 mrubinsk Exp $
+ * @author Michael Rubinsky <mrubinsk@horde.org>
+ */
+$theme_name = _("Aeros");
